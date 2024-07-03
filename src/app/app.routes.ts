0a1f52454923deb9596f8d762b118ea1e8789594
Fran Palmero <sweetydesigns@gmail.com>
2024-07-03 12:50:36 +0200

    Add Header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MapComponent } from './components/map/map.component';
+import { FullCalendarComponent } from './components/full-calendar/full-calendar.component';
+import { ChartsComponent } from './components/charts/charts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'map', component: MapComponent },
+  { path: 'calendar', component: FullCalendarComponent },
+  { path: 'charts', component: ChartsComponent },
+];
